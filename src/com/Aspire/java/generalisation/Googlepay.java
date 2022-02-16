54985916f082c9d97ab3d2d18e86c19f89914e67
pmmane214 <pmmane214@gmail.com>
2022-02-16 09:12:35 +0530

    16.02.22

--- src/com/Aspire/java/generalisation/Googlepay.java
+++ src/com/Aspire/java/generalisation/Googlepay.java
@@ -2,18 +2,6 @@ package com.Aspire.java.generalisation;
 
 public class Googlepay 
 {
-	public void sendMoney()
-	{
-		System.out.println("Google pay send money");
-	}
-	public void receivemoney()
-	{
-		
-	}
-	public void billpay()
-	{
-		
-	}
 	public void acCredit()
 	{
 		
